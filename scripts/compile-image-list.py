91d28b0df9b82d739bd1d41705c5513da99e7b7a
Nicolas Byl <nico@nicolas-byl.eu>
2019-06-04 10:44:03 +0200

    write image list

--- scripts/compile-image-list.py
+++ scripts/compile-image-list.py
@@ -1,11 +1,22 @@
 import argparse
+import array
+import io
+import yaml
 
-parser = argparse.ArgumentParser(description='Add some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='interger list')
-parser.add_argument('--sum', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+parser = argparse.ArgumentParser(description='Compile a list of images to retrieve from a .gitlab-ci.yml file.')
+parser.add_argument('--image-list', default='/tmp/pdk-image-list', help='an integer for the accumulator (default: /tmp/pdk-image-list)')
 args = parser.parse_args()
 
-print(args.sum(args.integers))
+images = {}
+with open(".gitlab-ci.yml", 'r') as stream:
+  gitlab_pipeline = yaml.safe_load(stream)
+
+  for job in gitlab_pipeline:
+    if isinstance(gitlab_pipeline[job],dict) and 'image' in gitlab_pipeline[job].keys():
+      images[gitlab_pipeline[job]['image']] = True
+
+with open(args.image_list, 'w') as image_list_file:
+  for image in images.keys():
+    image_list_file.write(image + '\n')
+
+print("Successfully wrote image list to " + args.image_list)
